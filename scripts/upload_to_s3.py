793cf0bad83f7c588372a958b484764f8913fad8
tungstennn <aqorane77@gmail.com>
2025-03-30 12:59:38 +0100

    Upload to s3

--- scripts/upload_to_s3.py
+++ scripts/upload_to_s3.py
@@ -1,23 +1,23 @@
 import boto3
 import os
 
-# 🔹 AWS S3 Configuration
+# Config
 S3_BUCKET = "ecom-sales-bucket"
-S3_FOLDER = "olist_data/"  # Folder in S3 where files will be stored
-LOCAL_FOLDER = "raw_data/"  # Folder where the dataset is stored locally
+S3_FOLDER = "olist_data/"
+LOCAL_FOLDER = "raw_data/"
 
-# 🔹 Initialize S3 Client
 s3 = boto3.client("s3")
 
-def upload_files():
-    """Uploads all files in raw_data/ to S3"""
+def upload_csv_files():
+    """Uploads CSVs in raw_data/ to S3."""
     for file_name in os.listdir(LOCAL_FOLDER):
-        file_path = os.path.join(LOCAL_FOLDER, file_name)
-        s3_key = f"{S3_FOLDER}{file_name}"  # Define S3 object key
+        if file_name.endswith(".csv"):
+            file_path = os.path.join(LOCAL_FOLDER, file_name)
+            s3_key = f"{S3_FOLDER}{file_name}"
 
-        print(f"📡 Uploading {file_name} to S3...")
-        s3.upload_file(file_path, S3_BUCKET, s3_key)
-        print(f"✅ {file_name} uploaded successfully!")
+            print(f"📤 Uploading {file_name} to S3...")
+            s3.upload_file(file_path, S3_BUCKET, s3_key)
+            print(f"✅ {file_name} uploaded successfully!")
 
 if __name__ == "__main__":
-    upload_files()
+    upload_csv_files()
