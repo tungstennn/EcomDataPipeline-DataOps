0242df7320f1a1b71c36bfdcae69abcfc6f2cc84
tungstennn <aqorane77@gmail.com>
2025-04-07 14:50:55 +0100

    Documenting code

--- scripts/extract_from_s3.py
+++ scripts/extract_from_s3.py
@@ -1,10 +1,10 @@
-import boto3
-import pandas as pd
-from io import BytesIO
+import boto3 # AWS SDK for Python
+import pandas as pd 
+from io import BytesIO 
 
-S3_BUCKET = 'ecom-sales-bucket'
-S3_PREFIX = 'olist_data/'
-S3_FILES = [
+S3_BUCKET = 'ecom-sales-bucket' # S3 bucket name
+S3_PREFIX = 'olist_data/'   # S3 folder name
+S3_FILES = [                # List of CSV files to extract
     'olist_orders_dataset.csv',
     'olist_customers_dataset.csv',
     'olist_order_items_dataset.csv',
@@ -16,6 +16,9 @@ S3_FILES = [
     'olist_geolocation_dataset.csv'
 ]
 
+
+
+# This function retrieves a CSV file from S3 and returns it as a pandas DataFrame
 def get_dataframe_from_s3(file_name):
     s3 = boto3.client('s3')
     key = f"{S3_PREFIX}{file_name}"
@@ -23,6 +26,9 @@ def get_dataframe_from_s3(file_name):
     content = response['Body'].read()
     return pd.read_csv(BytesIO(content))
 
+
+
+# This function extracts all CSV files from S3 and returns them as a dictionary of DataFrames
 def extract_all_data():
     data = {}
     for file in S3_FILES:
