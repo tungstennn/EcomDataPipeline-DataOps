e3249e34cf0e85a71fc88010c35a2831dc91bf80
tungstennn <aqorane77@gmail.com>
2025-04-07 14:52:38 +0100

    Documenting code

--- scripts/upload_to_s3.py
+++ scripts/upload_to_s3.py
@@ -1,17 +1,17 @@
-import boto3
+import boto3 # AWS SDK for Python
 import os
 
-# Config
-S3_BUCKET = "ecom-sales-bucket"
-S3_FOLDER = "olist_data/"
-LOCAL_FOLDER = "raw_data/"
+S3_BUCKET = "ecom-sales-bucket" # S3 bucket name
+S3_FOLDER = "olist_data/" # S3 folder name
+LOCAL_FOLDER = "raw_data/" # Local folder containing CSV files
 
-s3 = boto3.client("s3")
+s3 = boto3.client("s3") # Initialize S3 client
 
+# This function uploads all CSV files in the specified local folder to the specified S3 bucket and folder.
 def upload_csv_files():
     """Uploads CSVs in raw_data/ to S3."""
     for file_name in os.listdir(LOCAL_FOLDER):
-        if file_name.endswith(".csv"):
+        if file_name.endswith(".csv"): # Check if the file is a CSV
             file_path = os.path.join(LOCAL_FOLDER, file_name)
             s3_key = f"{S3_FOLDER}{file_name}"
 
@@ -20,4 +20,4 @@ def upload_csv_files():
             print(f"✅ {file_name} uploaded successfully!")
 
 if __name__ == "__main__":
-    upload_csv_files()
+    upload_csv_files() 
