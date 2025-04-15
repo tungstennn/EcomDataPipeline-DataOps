0a6c982767dd2e1622e7bd6736d8984e005c147b
tungstennn <aqorane77@gmail.com>
2025-04-15 15:42:11 +0100

    IAM role association fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,10 @@ module "s3" {
 }
 
 module "redshift" {
-  source = "./Redshift"
-  admin_username = var.admin_username
+  source              = "./Redshift"
+  admin_username      = var.admin_username
   admin_user_password = var.admin_user_password
+  iam_role_arn        = module.iam.iam_role_arn
 }
 
 module "iam" {
