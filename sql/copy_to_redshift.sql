56d5fb87a201dc8d933439f2cd1f70e757e84e6e
tungstennn <aqorane77@gmail.com>
2025-04-15 15:47:05 +0100

    COPY to redshift SQL

---
+++ sql/copy_to_redshift.sql
@@ -0,0 +1,75 @@
+-- COPY raw CSVs from S3 to staging tables in Redshift
+
+COPY stg_customers
+FROM 's3://ecom-sales-bucket/olist_data/olist_customers_dataset.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole'
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+COPY stg_geolocation
+FROM 's3://ecom-sales-bucket/olist_data/olist_geolocation_dataset.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole'
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+COPY stg_order_items
+FROM 's3://ecom-sales-bucket/olist_data/olist_order_items_dataset.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole'
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+COPY stg_order_payments
+FROM 's3://ecom-sales-bucket/olist_data/olist_order_payments_dataset.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole'
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+COPY stg_order_reviews
+FROM 's3://ecom-sales-bucket/olist_data/olist_order_reviews_dataset.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole' 
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+COPY stg_orders
+FROM 's3://ecom-sales-bucket/olist_data/olist_orders_dataset.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole'
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+COPY stg_products
+FROM 's3://ecom-sales-bucket/olist_data/olist_products_dataset.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole'
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+COPY stg_sellers
+FROM 's3://ecom-sales-bucket/olist_data/olist_sellers_dataset.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole'
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+COPY stg_product_category_name_translation
+FROM 's3://ecom-sales-bucket/olist_data/product_category_name_translation.csv'
+IAM_ROLE 'arn:aws:iam::329599652266:role/RedshiftS3AccessRole'
+FORMAT AS CSV
+IGNOREHEADER 1
+NULL AS ''
+REGION 'eu-west-2';
+
+
